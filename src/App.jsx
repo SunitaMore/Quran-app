8fd366c545d99ceffb62255e9ddbc91d360add09
Anurag <anuragtupkar168@gmail.com>
2025-04-08 12:02:17 +0530

    navbar component added in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+    <Navbar/>
       <Home/>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
