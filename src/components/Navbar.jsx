cd76aa9320e59d4ae073e12e52718f1483f52602
Tejashree <tejashreedevgharkar1116@gmail.com>
2025-05-19 03:28:55 +0530

    changes in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav className="bg-white shadow px-6 py-4 flex justify-between items-center">
+    <nav className="bg-white border-b border-gray-200 px-6 py-4 flex justify-between items-center">
       <h1 className="text-xl font-bold text-green-600">
         <Link to="/">Quran.co</Link>
       </h1>
       <ul className="hidden md:flex space-x-6 text-gray-700 font-medium">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
-        <li><Link to="/values">Values</Link></li>
+        <li><Link to="/values">How it's works </Link></li>
         <li><Link to="/tafsir">Tafsir</Link></li>
         <li><Link to="/translation">Translation</Link></li>
         <li><Link to="/donate">Donate</Link></li>
@@ -20,3 +20,4 @@ export default function Navbar() {
     </nav>
   );
 }
+
